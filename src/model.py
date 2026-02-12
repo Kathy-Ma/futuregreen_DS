dce25608fb09a61e44bcb19f4f15e97fe8b05863
InvertedCarrot <dyuan9378@gmail.com>
2026-02-12 06:42:57 -0500

    another update

--- src/model.py
+++ src/model.py
@@ -32,8 +32,16 @@ def standardized_image(img_path, img_size):
     """
     Given an image path, standardize the dimensions of an image
     Returns the image as an array with dimensions img_size and 3 color channels (RGB)
+
+    Args:
+        img_path: str - the path to the image to be standardized
+        img_size: tuple(int, int) - a tuple of (width, height) for the standardized image dimensions
     """
     img_array = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
+
+    # TODO: replace this functionality
+    # right now, we are just using cv2 to resize the image via stretching, but try experimenting with different image standardization techniques
+    # some examples: cropping, padding
     new_array = cv2.resize(img_array, img_size)
     return new_array
 
@@ -52,32 +60,67 @@ def create_data(img_size):
 
 
 
+def split_data(train_ratio, test_ratio, val_ratio, data_x, data_y):
+    """
+    Given train/test/validation ratios, split data_x and data_y according to those ratios
+    Args:
+        train_ratio, test_ratio, val_ratio: int - percentage of data to be split into training, testing, and validation
+        data_x: list - list of input features (images)
+        data_y: list - list of the image's corresponding labels
+    """
+    assert(train_ratio >= 0 and test_ratio >= 0 and val_ratio >= 0)
+    assert(train_ratio + test_ratio + val_ratio == 100)
+
+    # TODO: split the data according to the ratios, and return the split data as shown in the return statement
+    # the train_test_split function only splits into two groups, so we ned to call this function twice to split into three groups
+    # this fix just requires a bit of math
+    # alternatively, you can implement the splitting yourself without the sklearn function (remember to shuffle the data before splitting!)
+
+    state_num = 42
+
+    # split between train and test
+    xTrain, xTest, yTrain, yTest = train_test_split(
+        data_x,
+        data_y,
+        test_size = 0, # TODO: fix here
+        shuffle=True,
+        random_state = state_num
+    )
+
+    # TODO: split between train and validation (or test and validation, whichever you see fit)
+    if val_ratio != 0: # val_ratio = 0 => no validation set needed
+        xTrain, xVal, yTrain, yVal = train_test_split(
+            xTrain,
+            yTrain,
+            test_size = 0, # fix here
+            shuffle=True,
+            random_state = state_num
+        )
+    else:
+        xVal, yVal = [], []
+
+    return xTrain, xTest, xVal, yTrain, yTest, yVal
+
+
 
 if __name__ == "__main__":
     data_array_x, data_array_y = create_data(NORMALIZED_IMAGE_SIZE)
 
-    # encode class values (data_array_y)
+    # complete the conversion from category names -> numeric labels (integers) -> one-hot-encoding (0s and 1s)
     encoder = LabelEncoder()
-    encoder.fit(data_array_y_150) #learn the mapping between class names and integers
-    encoded_Y = encoder.transform(data_array_y_150)
-    dummy_y = to_categorical(encoded_Y)
+    encoder.fit(data_array_y)
+    encoded_Y = encoder.transform(data_array_y)
+    dummy_y = to_categorical(encoded_Y) # convert to one-hot-encoding labels
 
 
     # using sklearn to split data into Train/Val/Test
-    xTrain150, xTest150, yTrain, yTest = train_test_split(
-        np.asarray(data_array_x_150),  # convert to NumPy array
-        dummy_y,
-        test_size = 0.2,
-        shuffle=True,
-        random_state = 42)
+    xTrain, xTest, xVal, yTrain, yTest, yVal = split_data(
+        80, 10, 10,
+        np.asarray(data_array_x),
+        dummy_y
+    )
 
-    xTrain150, xVal150, yTrain, yVal = train_test_split(
-        xTrain150,
-        yTrain,
-        test_size = 0.2,
-        shuffle=True,
-        random_state = 42)
-
+# up to here now
 
     train_datagen = ImageDataGenerator(
         rescale=1./255,     #rescale the data in the range [0,1] from the varying range of each picture such that contribution in the loss value due to each picture can be equicalent
