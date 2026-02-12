466570ccd98f7337a80b900c99bd71d408c718b7
InvertedCarrot <dyuan9378@gmail.com>
2026-02-12 07:11:34 -0500

    more

--- src/model.py
+++ src/model.py
@@ -25,17 +25,18 @@ import pandas as pd
 
 
 NORMALIZED_IMAGE_SIZE = (150, 150)
-DATA_DIR = "../TrashNet/"
+DATA_DIR = "TrashNet/"
 CATEGORIES = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
 
 def standardized_image(img_path, img_size):
     """
     Given an image path, standardize the dimensions of an image
-    Returns the image as an array with dimensions img_size and 3 color channels (RGB)
 
     Args:
         img_path: str - the path to the image to be standardized
         img_size: tuple(int, int) - a tuple of (width, height) for the standardized image dimensions
+    Returns:
+        new_array: list - the standardized image as an array with dimensions img_size
     """
     img_array = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
 
@@ -63,10 +64,13 @@ def create_data(img_size):
 def split_data(train_ratio, test_ratio, val_ratio, data_x, data_y):
     """
     Given train/test/validation ratios, split data_x and data_y according to those ratios
+
     Args:
         train_ratio, test_ratio, val_ratio: int - percentage of data to be split into training, testing, and validation
         data_x: list - list of input features (images)
         data_y: list - list of the image's corresponding labels
+    Returns:
+        xTrain, xTest, xVal, yTrain, yTest, yVal: list - the splitted datasets
     """
     assert(train_ratio >= 0 and test_ratio >= 0 and val_ratio >= 0)
     assert(train_ratio + test_ratio + val_ratio == 100)
@@ -82,17 +86,17 @@ def split_data(train_ratio, test_ratio, val_ratio, data_x, data_y):
     xTrain, xTest, yTrain, yTest = train_test_split(
         data_x,
         data_y,
-        test_size = 0, # TODO: fix here
+        test_size = 0.2, # TODO: fix here
         shuffle=True,
         random_state = state_num
     )
 
     # TODO: split between train and validation (or test and validation, whichever you see fit)
     if val_ratio != 0: # val_ratio = 0 => no validation set needed
-        xTrain, xVal, yTrain, yVal = train_test_split(
-            xTrain,
-            yTrain,
-            test_size = 0, # fix here
+        xTest, xVal, yTest, yVal = train_test_split(
+            xTest,
+            yTest,
+            test_size = 0.5, # fix here
             shuffle=True,
             random_state = state_num
         )
@@ -101,6 +105,14 @@ def split_data(train_ratio, test_ratio, val_ratio, data_x, data_y):
 
     return xTrain, xTest, xVal, yTrain, yTest, yVal
 
+def augment_data(xTrain, yTrain):
+    '''
+    Given training data, augment the training data with a data generator's transformations
+
+    Args:
+        xTrain: list - list of input features (images) for the training set
+        yTrain: list - list of the image's corresponding labels for the training set
+    '''
 
 
 if __name__ == "__main__":
@@ -113,35 +125,55 @@ if __name__ == "__main__":
     dummy_y = to_categorical(encoded_Y) # convert to one-hot-encoding labels
 
 
-    # using sklearn to split data into Train/Val/Test
+    # split data into train/test/val sets
     xTrain, xTest, xVal, yTrain, yTest, yVal = split_data(
         80, 10, 10,
         np.asarray(data_array_x),
         dummy_y
     )
 
+    def create_data_generator(data_x, data_y, batch_size=32, only_normalize=False):
+        '''
+        Given data_x and data_y, set up a data generator for the data
+
+        Args:
+            data_x: list - list of input features (images)
+            data_y: list - list of the image's corresponding labels
+            batch_size: int - the number of images to be yielded from the generator per batch
+            only_normalize: bool - whether to only apply rescaling to the data, or to apply other transformations as well
+        Returns:
+            datagen: ImageDataGenerator - the data generator with the specified transformations, after attaching the datasets to it
+        '''
+
+        if only_normalize:
+            # only normalize the data (used for testing/validation data)
+            datagen = ImageDataGenerator(rescale=1./255)
+        else:
+            # else, apply other transformations
+            datagen = ImageDataGenerator(
+                # TODO: experiment with different transformations and their parameters, and see what works best
+                # you can also try other transformations not listed here
+
+                rescale=1./255,     #rescale the data in the range [0,1] from the varying range of each picture such that contribution in the loss value due to each picture can be equicalent
+                shear_range=0.2,    #Basically think of it as converting a square to a rhombus
+                zoom_range=0.2,     #Randomly zooming inside pictures
+                # width_shift_range=0.2, #It actually shift the image to the left or right(horizontal shifts). If the value is float and <=1 it will take the percentage of total width as range. Suppose image width is 100px. if width_shift_range = 1.0 it will take -100% to +100% means -100px to +100px. It will shift image randomly between this range. Randomly selected positive value will shift the image to the right side and negative value will shift the image to the left side.
+                rotation_range=10,
+                # height_shift_range=0.2, #shifts the image vertically
+                # horizontal_flip=True, #flips both rows and columns horizontally
+                # vertical_flip=True, #flips both rows and columns vertically
+            )
+        
+        # flow() hooks up the training data, which tells the data generator which data to apply transformations to
+        generator = datagen.flow(data_x, data_y, batch_size=batch_size, shuffle=True)
+        return generator
+
+    # set up generators to use later for training and evaluation
+    train_generator = create_data_generator(xTrain, yTrain, batch_size=32, only_normalize=False)
+    test_generator = create_data_generator(xTest, yTest, batch_size=32, only_normalize=True)
+    val_generator = create_data_generator(xVal, yVal, batch_size=32, only_normalize=True)
+
 # up to here now
-
-    train_datagen = ImageDataGenerator(
-        rescale=1./255,     #rescale the data in the range [0,1] from the varying range of each picture such that contribution in the loss value due to each picture can be equicalent
-        shear_range=0.2,    #Basically think of it as converting a square to a rhombus
-        zoom_range=0.2,     #Randomly zooming inside pictures
-        # width_shift_range=0.2, #It actually shift the image to the left or right(horizontal shifts). If the value is float and <=1 it will take the percentage of total width as range. Suppose image width is 100px. if width_shift_range = 1.0 it will take -100% to +100% means -100px to +100px. It will shift image randomly between this range. Randomly selected positive value will shift the image to the right side and negative value will shift the image to the left side.
-        rotation_range=10,
-        # height_shift_range=0.2, #shifts the image vertically
-        # horizontal_flip=True, #flips both rows and columns horizontally
-        # vertical_flip=True, #flips both rows and columns vertically
-    )
-
-    train_generator_150 = train_datagen.flow(
-        xTrain150,
-        yTrain,
-        batch_size=32, #No. of images to be yielded from the generator per batch
-        shuffle=True
-    )
-
-    val_generator_150 = ImageDataGenerator(rescale=1./255).flow(xVal150, yVal, batch_size=32)
-    test_generator_150 = ImageDataGenerator(rescale=1./255)
 
     # plot training and validation curves (Accuracy and Loss)
     def plot_history(model_history):
