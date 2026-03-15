0260c1f586278674d52f12f96a4b8a60318103a4
InvertedCarrot <dyuan9378@gmail.com>
2026-03-15 17:48:40 -0400

    setup for testing done

--- src/main_files/main.py
+++ src/main_files/main.py
@@ -7,55 +7,103 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 
 
+
 from dataset_manager import DatasetManager
 from model import GarbageClassificationModel
 from transfer_learning_models.resnet50_model import ResNet50Model
 from transfer_learning_models.mobilenetv3_model import MobileNetV3Model
 from transfer_learning_models.nasnetmobile_model import NASNetMobileModel
+
 from logger import Logger
 
 
 
 if __name__ == "__main__":
-    DATASET_DIR = "datasets/dataset_vers1"
-    NORMALIZED_IMAGE_SIZE = (150, 150)
-    MODEL_NAMES = [
-        "benchmark_model",
-        "resnet50_model",
-        "mobilenetv3_model",
-        "nasnetmobile_model"
-    ] # this acts as the export name
-    MODEL_CLASSES = [
-        GarbageClassificationModel, 
-        ResNet50Model, 
-        MobileNetV3Model, 
-        NASNetMobileModel
-    ]
-    NUM_EPOCHS = [50, 25, 25, 25]
-    
-    for model_name, model_class, num_epochs in zip(MODEL_NAMES, MODEL_CLASSES, NUM_EPOCHS):
-        # set up all variables
-        # (this order is important!)
-        logger = Logger(model_name)
-        dl = DatasetManager(DATASET_DIR, NORMALIZED_IMAGE_SIZE, logger=logger)
-        m = model_class(dl, logger=logger)
+    DATASET_DIR_1 = "datasets/benchmark_dataset/"
+    NORMALIZED_IMAGE_SIZE_1 = (150, 150)
 
-        # prepare the dataset for the model to use
-        dl.load_data()
-        dl.split_data(60, 20, 20)
+    # using the base model architecture =============================================================================
+    # initialize the dataset loader and get the split datasets
+    # dl = DatasetManager(DATASET_DIR_1, NORMALIZED_IMAGE_SIZE_1)
+    # dl.load_data()
+    # dl.split_data(60, 20, 20)
 
-        # train the model
-        m.load_model_from_file(f"model_registry/{model_name}.keras")
-        # model_history = m.train_model(
-        #     epochs=num_epochs,
-        #     batch_size=32,
-        #     export_path=f"model_registry/{model_name}.keras"
-        # )
-        # m.plot_history(model_history)
-        m.measure_metrics()
-        # m.predict_img("datasets/benchmark_dataset/organics/organics_1.jpg")
-        m.random_preds(3, 6)
-        m.random_preds(8, 4)
-        m.random_preds(10, 10)
+    # # the base model (not good but also not bad, ~60% accuracy at 25 epochs)
+    # m = GarbageClassificationModel(dl)
+    # model_history = m.train_model(
+    #     epochs=25,
+    #     batch_size=32,
+    #     export_path="model_registry/benchmark_model_2.keras"
+    # )
+    # m.plot_history(model_history)
+    # m.measure_metrics()
+
+    # using transfer learning (ResNet50 in this example) =============================================================================
+    # DATASET_DIR_2 = "datasets/dataset_vers1/"
+    # NORMALIZED_IMAGE_SIZE_2 = (150, 150)
+
+    # MODEL_PATH_1 = "model_registry/resnet_model.keras"
     
-    # TODO: for tomorrow, try loading in all the trained models and make sure that's actually possible to do
+    # dl2 = DatasetManager(DATASET_DIR_2, NORMALIZED_IMAGE_SIZE_2)
+    # dl2.load_data()
+    # dl2.split_data(60, 20, 20)
+
+    # m2 = ResNet50Model(dl2)
+    # model_history = m2.train_model(
+    #     epochs=10,
+    #     batch_size=32,
+    #     export_path=MODEL_PATH_1
+    # )
+    # m2.plot_history(model_history)
+    # m2.measure_metrics()
+
+    # # reload the model
+    # m22 = GarbageClassificationModel(dl2)
+    # m22.load_model_from_file(MODEL_PATH_1)
+    # m22.measure_metrics()
+
+    DATASET_DIR = "datasets/dataset_vers1/"
+    # NORMALIZED_IMAGE_SIZE = (150, 150)
+
+    model_classes = [MobileNetV3Model, NASNetMobileModel]
+    image_sizes = [150, 175, 200, 225, 250, 275, 300]
+    
+    for model_class in model_classes:
+        for image_dim in image_sizes:
+            img_size = (image_dim, image_dim)
+            export_name = f"{model_class.__name__}_imgsize_{img_size}"
+            logger = Logger(export_name)
+            dl = DatasetManager(DATASET_DIR, img_size, logger=logger)
+            dl.load_data()
+            dl.split_data(60, 20, 20)
+
+            m = model_class(dl, logger=logger)
+            model_history = m.train_model(
+                epochs=25,
+                batch_size=32,
+                export_path=f"model_registry/{export_name}.keras"
+            )
+            m.plot_history(model_history)
+            m.measure_metrics()
+            m.predict_img("datasets/dataset_vers1/organic/organic_1.jpg")
+            m.random_preds(3, 6)
+            m.random_preds(8, 4)
+    
+
+    # logger = Logger("demo_model")
+    # dl = DatasetManager(DATASET_DIR, NORMALIZED_IMAGE_SIZE, logger=logger)
+    # dl.load_data()
+    # dl.split_data(60, 20, 20)
+    # m = GarbageClassificationModel(dl, logger=logger)
+    # m.load_model_from_file("model_registry/resnet50_model.keras")
+    # # model_history = m.train_model(
+    # #     epochs=5,
+    # #     batch_size=32,
+    # #     export_path="model_registry/demo_model.keras"
+    # # )
+    # # m.plot_history(model_history)
+    # m.measure_metrics()
+    # m.predict_img("datasets/benchmark_dataset/organics/organics_1.jpg")
+    # m.random_preds(3, 6)
+    # m.random_preds(8, 4)
+    # m.random_preds(10, 10)
